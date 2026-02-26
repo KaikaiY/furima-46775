b4a0e2b946206d94cc9458d6f5f7a5a4c79fe22a
KaikaiY <porter.kai222@gmail.com>
2026-02-26 14:07:20 +0900

    column_change(null_ture)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_26_024059) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_26_050015) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_024059) do
     t.string "last_name_kanji", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.date "birthday", null: false
+    t.date "birthday"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
