6a21b15f3ca7ee9aad06039d568bf6e5a9ef7333
KaikaiY <porter.kai222@gmail.com>
2026-03-04 14:32:00 +0900

    ItemsModelCreate

---
+++ db/migrate/20260304052015_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string     :item_name,        null: false
+      t.text       :item_description, null: false
+      t.integer    :category_id,      null: false
+      t.integer    :condition_id,     null: false
+      t.integer    :fee_id,           null: false
+      t.integer    :prefecture_id,    null: false
+      t.integer    :delivery_id,      null: false
+      t.bigint     :price,            null: false
+      t.references :user,             null: false, foreign_key: :user_id
+      
+      t.timestamps
+    end
+  end
+end
