20915cb78f58a45073e28d8ed534116d63bac316
KaikaiY <porter.kai222@gmail.com>
2026-03-04 11:56:33 +0900

    ValidationEdit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,9 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name_kanji
     validates :last_name_kanji
-    validates :first_name_kana
-    validates :last_name_kana
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
