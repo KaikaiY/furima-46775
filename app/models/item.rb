f1b56cd516102694739349d896eba1b77b428126
KaikaiY <porter.kai222@gmail.com>
2026-03-05 10:25:46 +0900

    items_db_create

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :fee, :prefecture, :delivery
+  has_one_attached :image
+
+  validates :category_id, :condition_id, :fee_id, :prefecture_id, :delivery_id,
+            numericality: { other_than: 1, message: "can't be blank" }
 end
